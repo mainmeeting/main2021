34d368b76a425ec7c700bdd5e9da2cf55ab9fbe0
arvkumar <arvind.k.panchal@gmail.com>
2021-10-11 17:05:57 +0200

    Oranizers

---
+++ organizers/organizers.component.js
@@ -0,0 +1,6 @@
+// Register the `organizers` component on the `organizers` module,
+angular.
+module('organizers').
+component('organizers', {
+    templateUrl: 'organizers/organizers.template.html',
+});
